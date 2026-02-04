e08ec77144731927c0a29ec8a9f8ab389824a9d6
Vadim <vadimchubok@gmail.com>
2026-02-04 18:07:54 +0200

    feat(movies): add SQLAlchemy models for actors and directors

--- src/movies/models.py
+++ src/movies/models.py
@@ -23,6 +23,20 @@ movie_genres = Table(
     Column("genre_id", ForeignKey("genres.id"), primary_key=True),
 )
 
+movie_directors = Table(
+    "movie_directors",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
+    Column("director_id", ForeignKey("directors.id"), primary_key=True),
+)
+
+movie_stars = Table(
+    "movie_stars",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
+    Column("star_id", ForeignKey("stars.id"), primary_key=True),
+)
+
 
 class Genre(Base):
     __tablename__ = "genres"
@@ -33,4 +47,28 @@ class Genre(Base):
     movies: Mapped[List["Movie"]] = relationship(
         secondary=movie_genres,
         back_populates="genres",
+    )
+
+
+class Star(Base):
+    __tablename__ = "stars"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    movies: Mapped[List["Movie"]] = relationship(
+        secondary=movie_stars,
+        back_populates="stars",
+    )
+
+
+class Director(Base):
+    __tablename__ = "directors"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    movies: Mapped[List["Movie"]] = relationship(
+        secondary=movie_directors,
+        back_populates="directors",
     )
