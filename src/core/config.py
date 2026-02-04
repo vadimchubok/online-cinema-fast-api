1425caf06ef18ded5a76becf400367a14e193d0f
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-02-04 22:04:44 +0200

    feat(users): add JWT and email configuration to Settings

--- src/core/config.py
+++ src/core/config.py
@@ -11,8 +11,9 @@ class Settings(BaseSettings):
     # JWT Settings
     SECRET_KEY_ACCESS: str
     SECRET_KEY_REFRESH: str
-    JWT_SIGNING_ALGORITHM: str
-
+    JWT_SIGNING_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     REDIS_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
