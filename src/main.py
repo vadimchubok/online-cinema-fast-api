8140b765b2d98494842e34ab3534f295e6710114
Andrii Yermolenko <vakt159@gmail.com>
2026-02-06 15:09:06 +0200

    Add router to main

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ from src.auth.router import router as auth_router
 from src.movies.routers import router as movies_router
 from src.interactions.router import router as interaction_router
 
+from src.cart.routers import router as cart_router
 
 app = FastAPI(title="Online Cinema API")
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(movies_router, prefix="/api/v1")
 app.include_router(interaction_router, prefix="/api/v1")
 
+app.include_router(cart_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
