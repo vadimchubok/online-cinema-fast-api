34dc2a71590f1c13401c82f298a6115438a34f3e
Vadim <vadimchubok@gmail.com>
2026-02-06 14:30:51 +0200

    feat(genres-actors): added routers to main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from src.auth.router import router as auth_router
 from src.movies.routers import router as movies_router
+from src.movies.routers import genres_router as genres_router
+from src.movies.routers import stars_router as stars_router
 from src.interactions.router import router as interaction_router
 
 
 app = FastAPI(title="Online Cinema API")
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(movies_router, prefix="/api/v1")
+app.include_router(genres_router, prefix="/api/v1")
+app.include_router(stars_router, prefix="/api/v1")
 app.include_router(interaction_router, prefix="/api/v1")
 
 
